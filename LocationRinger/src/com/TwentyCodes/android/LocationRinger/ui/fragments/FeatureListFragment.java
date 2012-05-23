d2c0e35a1bb45bfa1ccb13bcbaa4d66538052a99
Ricky Barrette <rickbarrette@gmail.com>
2012-05-22 21:48:33 -0400

    Added some simple add/remove methods, and I removed the FeatureListAdaptor
    Signed-off-by: Ricky Barrette <rickbarrette@gmail.com>

--- LocationRinger/src/com/TwentyCodes/android/LocationRinger/ui/fragments/FeatureListFragment.java
+++ LocationRinger/src/com/TwentyCodes/android/LocationRinger/ui/fragments/FeatureListFragment.java
@@ -11,9 +11,7 @@ import java.util.ArrayList;
 import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,22 +20,26 @@ import android.view.ViewGroup;
 import com.TwentyCodes.android.LocationRinger.OnContentChangedListener;
 import com.TwentyCodes.android.LocationRinger.R;
 import com.TwentyCodes.android.LocationRinger.debug.Debug;
-import com.TwentyCodes.android.LocationRinger.ui.fragments.FragmentListAdaptor.Holder;
 
 /**
  * This fragment will be used to display a list of fragments
- * 
- * TODO + create button bar that had a plus button and a hint + add/remove
- * features
+ * TODO 
+ * + create button bar that had a plus button and a hint + add/remove features
  * 
  * @author ricky
  */
 public class FeatureListFragment extends Fragment {
 
-
 	private static final String TAG = "FeatureListFragment";
 	private final ArrayList<Fragment> mFragments;
 
+	/**
+	 * Creates a new FeatureListFragment
+	 * @param info
+	 * @param listener
+	 * @param fragments
+	 * @author ricky barrette
+	 */
 	public FeatureListFragment(ContentValues info, OnContentChangedListener listener, ArrayList<Fragment> fragments) {
 		super();
 		mFragments = fragments;
@@ -51,8 +53,6 @@ public class FeatureListFragment extends Fragment {
 	 */
 	@Override
 	public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle bundle) {
-		// TODO Auto-generated method stub
-		
 		return inflator.inflate(R.layout.fragment_list_contianer, null);
 	}
 
@@ -60,13 +60,16 @@ public class FeatureListFragment extends Fragment {
 	public void onResume() {
 		if (Debug.DEBUG)
 			Log.v(TAG, "onResume()");
-		
 		loadFragments();
 		super.onResume();
 	}
 
+	/**
+	 * Loads all the fragments
+	 * @author ricky barrette
+	 */
 	private void loadFragments() {
-		FragmentTransaction transaction = this.getFragmentManager().beginTransaction();
+		final FragmentTransaction transaction = this.getFragmentManager().beginTransaction();
 		for(Fragment fragment : this.mFragments){
             transaction.add(R.id.fragment_list_contianer, fragment, fragment.getTag());
 		}
@@ -74,11 +77,26 @@ public class FeatureListFragment extends Fragment {
 	}
 	
 	/**
-	 * Simple Holder class
+	 * Adds the fragment to the list
+	 * @param fragment
 	 * @author ricky barrette
 	 */
-	class Holder{
-		public String tag;
-		public View view;
+	public void add(final Fragment fragment){
+		this.mFragments.add(fragment);
+		final FragmentTransaction transaction = this.getFragmentManager().beginTransaction();
+		transaction.add(R.id.fragment_list_contianer, fragment, fragment.getTag());
+		transaction.commit();
+	}
+	
+	/**
+	 * Removes a fragment from the list
+	 * @param fragment
+	 * @author ricky barrette
+	 */
+	public void remove(final Fragment fragment){
+		this.mFragments.remove(fragment);
+		final FragmentTransaction transaction = this.getFragmentManager().beginTransaction();
+		transaction.remove(fragment);
+		transaction.commit();
 	}
 }
